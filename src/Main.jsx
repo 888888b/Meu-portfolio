83c831dab96763d31da49b3cf98ea1b08b10a841
888888b <Vitorhugo621contato@gmail.com>
2023-12-21 20:52:59 +0000

    responsividade

--- src/Main.jsx
+++ src/Main.jsx
@@ -2,6 +2,11 @@ import React, { useEffect } from 'react';
 import imgDev from "../src/images/img-dev.jpg";
 import imgPerfil from "../src/images/foto-perfil.png";
 import AOS from "aos";
+import html from "../src/images/html-log.png";
+import js from '../src/images/js-loogo.png';
+import js2 from "../src/images/js-logo.webp";
+import css from "../src/images/css-log.png";
+import react from "../src/images/react-logo.png";
 import 'aos/dist/aos.css';
 
 
@@ -30,8 +35,8 @@ function Main(){
       <div className="div-sobre">
         <div id="div-sobre-text">
           <h2>Sobre min</h2>
-          <h1>Sou um Desenvolvedor <br/>Web</h1>
-          <p>Desde criança sou fascinado pela maneira como são criados programas e sistemas, e depois de anos aqui estou , na programação!</p>
+          <h1>Explorando o mundo digital</h1>
+          <p>Desde criança sou fascinado pela tecnologia e sua transformação. Atualmente, sigo meus estudos na área da programação web.</p>
         </div>
 
         <div id="div-sobre-img">
@@ -40,23 +45,16 @@ function Main(){
         
       </div>
 
-      <div className="div-experiencia">
-        <div className="experiencia-titulo"><h2>Meu resumo</h2></div>
-        <div className="experiencia-divs">
-          <div className="experiencia-text">
-            <h2>Conhecimento</h2>
-            <h1>A mais de 1 ano como um <span>Desenvolvedor</span> </h1>
-          </div>
-          <div className="div-estudos">
-            <ul>
-              <li>Java Script</li>
-              <p>1 ano de experiencia</p>
-              <li>HTML e CSS</li>
-              <p>1.5 anos de experiencia</p>
-              <li>Python</li>
-              <p>1 ano de experiencia</p>
-            </ul>
-          </div>
+      <div className='experiencia'>
+        <div className='experiencia-text'>
+          <h1>Linguagens e Frameworks</h1>
+          <h2>E do meu conhecimento as tecnologias:</h2>
+        </div>
+        <div className='experiencia-icons'>
+          <div className='linguagem'><img src={js2}/></div>
+          <div className='linguagem'><img id="html" src={html}/></div>
+          <div className='linguagem'><img src={css}/></div>
+          <div className='linguagem'><img src={react}/></div>
         </div>
       </div>
     </main>
